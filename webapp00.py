54a462dedf1a1b1bb431053cc99c1d6ce3798c79
Ryan-Jori <79109999+Ryan-Jori@users.noreply.github.com>
2022-09-21 19:14:49 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -2,6 +2,7 @@
 #Bibliotecas
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
   
 # Título
 st.title("Cálculo CCE - Dólar")
@@ -9,8 +10,9 @@ st.title("Cálculo CCE - Dólar")
 # Descrição do Projeto
 st.header("A finalidade deste projeto é calcular o lucro baseado na redução da emissão de gases nocivos às condições globais.")
 
+
 # Use st.write("") para adicionar um texto ao seu Web app
-st.write("Insira abaixo as informações necessárias para o cálculo.")
+st.write("Um crédito de carbono equivale a R$175,00")
 
 #Coluna Informativa relacionada ao ganho de créditos relacionados à emissão reduzida.
 col1, col2 = st.columns(2)
@@ -25,6 +27,9 @@ col5, col6 = st.columns(2)
 col1.write("Hexafluoreto de Enxofre")
 col2.write("23900 Créditos por Tonelada")
 
+# Use st.write("") para adicionar um texto ao seu Web app
+st.write("Insira abaixo as informações necessárias para o cálculo.")
+
 # Selecionar o gás.
 option = st.selectbox(
     'Qual gás nocivo deseja reduzir?',
@@ -38,12 +43,29 @@ if st.button('Consultar'):
   if option == 'CO2 (Carbono)':
     st.write(number, "toneladas conferem", number, "créditos")  
     
- 
+    
   if option == 'CH4 (Gás Metano)':
     st.write(number, "toneladas conferem", number*21, "créditos")
-  
+    number = 21*number
+    
   if option == 'SF6 (Hexafluoreto de Enxofre)':
     st.write(number, "toneladas conferem", number*23900, "créditos")
+    number = 23900*number
+  
+  
+ # Gráfico representando a proporção e distribuição do ganho por tonelada de gás reduzido.
+  st.write("Valor acarretado por céditos de Carbono :", number)
+  
+  labels = 'Doações', 'Recebido pela empresa', 'Serviço'
+  sizes = [(number/100)*10, (number/100)*80, (number/100)*10]
+  explode = (0, 0.1, 0, )  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+  fig1, ax1 = plt.subplots()
+  ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+  ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+  st.pyplot(fig1)
 
   
 #Proporção crédito de carbono/dólar
