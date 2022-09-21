1c31f3e9c0895de9d61948fb6d42a151db4dcdf2
Ryan-Jori <79109999+Ryan-Jori@users.noreply.github.com>
2022-09-21 12:01:57 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -23,13 +23,13 @@ option = st.selectbox(
 st.write('Você selecionou ', option)
 
 #Proporção gás/crédito de carbono
-if option == 'CO2 (Carbono)'
+if option == 'CO2 (Carbono)':
   st.write('Uma tonelada deemissão de carbono reduzida equivale a 1 Crédito de carbono')
  
-if option == 'CH4 (Gás Metano)'
+if option == 'CH4 (Gás Metano)':
   st.write('Uma tonelada deemissão de carbono reduzida equivale a 21 Créditos de carbono')
   
-if option == 'SF6 (Hexafluoreto de Enxofre)'
+if option == 'SF6 (Hexafluoreto de Enxofre)':
   st.write('Uma tonelada deemissão de carbono reduzida equivale a 23900 Créditos de carbono')
 
 #Proporção crédito de carbono/dólar
