21f7f2fdbec650601b917d082df367b843cb19d6
Ryan-Jori <79109999+Ryan-Jori@users.noreply.github.com>
2022-09-21 18:17:23 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -17,11 +17,11 @@ col1, col2 = st.columns(2)
 col1.write("Carbono")
 col2.write("1 Crédito por Tonelada")
 
-col3, col5 = st.columns2(2)
+col3, col5 = st.columns(2)
 col1.write("Metano")
 col2.write("21 Créditos por Tonelada")
 
-col5, col6 = st.columns3(2)
+col5, col6 = st.columns(2)
 col1.write("Hexafluoreto de Enxofre")
 col2.write("23900 Créditos por Tonelada")
 
