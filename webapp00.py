9f4ec2d03cb50f76da92a5199e45c97ebbcc7762
Ryan-Jori <79109999+Ryan-Jori@users.noreply.github.com>
2022-09-21 18:33:56 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -12,7 +12,7 @@ st.header("A finalidade deste projeto é calcular o lucro baseado na redução d
 # Use st.write("") para adicionar um texto ao seu Web app
 st.write("Insira abaixo as informações necessárias para o cálculo.")
 
-'''
+#Coluna Informativa relacionada ao ganho de créditos relacionados à emissão reduzida.
 col1, col2 = st.columns(2)
 col1.write("Carbono")
 col2.write("1 Crédito por Tonelada")
@@ -24,35 +24,26 @@ col2.write("21 Créditos por Tonelada")
 col5, col6 = st.columns(2)
 col1.write("Hexafluoreto de Enxofre")
 col2.write("23900 Créditos por Tonelada")
-'''
 
-tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
-tab1.write("Carbono")
-tab2.write("1 Crédito por Tonelada")
-
-tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
-tab1.write("Metano")
-tab2.write("21 Créditos por Tonelada")
-
-tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
-tab1.write("Hexafluoreto de Enxofre")
-tab2.write("23900 Créditos por Tonelada")
-
-
-# Selecionar o gás
+# Selecionar o gás.
 option = st.selectbox(
     'Qual gás nocivo deseja reduzir?',
     ('CO2 (Carbono)', 'CH4 (Gás Metano)', 'SF6 (Hexafluoreto de Enxofre)'))
 
-#Proporção gás/crédito de carbono
-if option == 'CO2 (Carbono)':
-  st.write('Uma tonelada de emissão de carbono reduzida equivale a 1 Crédito de carbono')
+#Coleção dos dados relacionados à quantidade reduzida.
+number = st.number_input('insira o valor em Toneladas')
+
+#Botão que aciona o cálculo de proporção de ganho pela quantia reduzida da emissão.
+if st.button('Consultar'):
+  if option == 'CO2 (Carbono)':
+    
  
 if option == 'CH4 (Gás Metano)':
-  st.write('Uma tonelada de emissão de carbono reduzida equivale a 21 Créditos de carbono')
+
   
 if option == 'SF6 (Hexafluoreto de Enxofre)':
-  st.write('Uma tonelada de emissão de carbono reduzida equivale a 23900 Créditos de carbono')
+
+
   
 #Proporção crédito de carbono/dólar
 
