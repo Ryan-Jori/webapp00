8dc2a7441dd7b4333f08a92d62c330d6c8c8838f
Ryan-Jori <79109999+Ryan-Jori@users.noreply.github.com>
2022-09-21 11:57:58 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -15,11 +15,23 @@ st.subheader("Sub Cabeçalho")
 # Use st.write("") para adicionar um texto ao seu Web app
 st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
 
-# Selecionar 
+# Selecionar o gás
 option = st.selectbox(
     'Qual gás nocivo deseja reduzir?',
     ('CO2(Carbono)', 'CH4(Gás Metano)', 'SF6 (Hexafluoreto de Enxofre)'))
 
-st.write('Você selecionou :', option)
+st.write('Você selecionou ', option)
+
+#Proporção gás/crédito de carbono
+if option == CO2(Carbono)
+  st.write('Uma tonelada deemissão de carbono reduzida equivale a 1 Crédito de carbono')
+ 
+if option == CH4(Gás Metano)
+  st.write('Uma tonelada deemissão de carbono reduzida equivale a 21 Créditos de carbono')
+  
+if option == SF6 (Hexafluoreto de Enxofre)
+  st.write('Uma tonelada deemissão de carbono reduzida equivale a 23900 Créditos de carbono')
+
+#Proporção crédito de carbono/dólar
 
 
