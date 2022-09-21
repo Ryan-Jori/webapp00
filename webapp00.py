99d3319d94e3f1953702ead7161478bcf277d522
Ryan-Jori <79109999+Ryan-Jori@users.noreply.github.com>
2022-09-21 18:16:49 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -14,8 +14,17 @@ st.write("Insira abaixo as informações necessárias para o cálculo.")
 
  
 col1, col2 = st.columns(2)
-col1.write("This is column 1")
-col2.write("This is column 2")
+col1.write("Carbono")
+col2.write("1 Crédito por Tonelada")
+
+col3, col5 = st.columns2(2)
+col1.write("Metano")
+col2.write("21 Créditos por Tonelada")
+
+col5, col6 = st.columns3(2)
+col1.write("Hexafluoreto de Enxofre")
+col2.write("23900 Créditos por Tonelada")
+
 
 # Selecionar o gás
 option = st.selectbox(
@@ -24,13 +33,13 @@ option = st.selectbox(
 
 #Proporção gás/crédito de carbono
 if option == 'CO2 (Carbono)':
-  st.write('Uma tonelada deemissão de carbono reduzida equivale a 1 Crédito de carbono')
+  st.write('Uma tonelada de emissão de carbono reduzida equivale a 1 Crédito de carbono')
  
 if option == 'CH4 (Gás Metano)':
-  st.write('Uma tonelada deemissão de carbono reduzida equivale a 21 Créditos de carbono')
+  st.write('Uma tonelada de emissão de carbono reduzida equivale a 21 Créditos de carbono')
   
 if option == 'SF6 (Hexafluoreto de Enxofre)':
-  st.write('Uma tonelada deemissão de carbono reduzida equivale a 23900 Créditos de carbono')
+  st.write('Uma tonelada de emissão de carbono reduzida equivale a 23900 Créditos de carbono')
   
 #Proporção crédito de carbono/dólar
 
