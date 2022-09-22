8bd4326f129bb921eeecea3076e102a7ee0eef98
Ryan-Jori <79109999+Ryan-Jori@users.noreply.github.com>
2022-09-22 08:06:22 -0300

    Update webapp01.py

--- webapp01.py
+++ webapp01.py
@@ -6,11 +6,12 @@ import streamlit as st
 from PIL import Image
 
 #Separação de Abas
-tab1, tab2, tab3 = st.tabs([st.image('https://media.discordapp.net/attachments/1021891230868717588/1022296227645239306/ecodoalogo-removebg-preview.png'), "Informações", "Calculadora"])
+tab1, tab2, tab3 = st.tabs("Sobre", "Informações", "Calculadora"])
 
 #------------------------------------------------------------------------ Primeira Aba
 with tab1:
     # Título
+    st.image('https://media.discordapp.net/attachments/1021891230868717588/1022296227645239306/ecodoalogo-removebg-preview.png', width=400)
     st.title("EcoDoa")
 
     # Descrição do Projeto
