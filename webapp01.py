c31ec8737a482af8ece80824a80a70f82c24393c
Ryan-Jori <79109999+Ryan-Jori@users.noreply.github.com>
2022-09-22 16:42:29 -0300

    Update webapp01.py

--- webapp01.py
+++ webapp01.py
@@ -46,7 +46,6 @@ with tab2:
     # Use st.write("") para adicionar um texto ao seu Web app
     st.header('Créditos de Carbono')
     st.write('Crédito de carbono é um conceito surgido no fim dos anos 90, visando reduzir a emissão de gáses que intensificam o efeito estufa e combater o grande problema das mudanças climáticas. A ideia surgiu no Protocolo de Kyoto, dizendo que cada tonelada de carbono ou outros gases reduzida por um país, uma certificação é emitida pelo MDL (Mecanismo de Desenvolvimento Limpo).')
-    st.write(' ')
     st.write('Sendo assim, quanto mais reduzida é a emissão de carbono de um país, mais crédito se obtem. Atualmente, pode-se utilizar esta certificação como moeda com outros países que não tenham atingido sua meta anual estipulada. Existem diversas estratégias  para a redução destes compostos extremamente nocivos ao ambiente, tais como :')
     st.write('- Redução dos níveis de desmatamento')
     st.write('- Utilização de fontes de energia alternativas e mais limpas')
@@ -54,7 +53,10 @@ with tab2:
     st.write('O Brasil, por exemplo, consta entre os maiores emissores destes gases, provenientes do corte excessivo de arvores e desmatamento desenfreado. Nosso país consta como o 4º na lista das nações que mais poluem o ambiente desta forma, com 5% do total mundial desde o século 19.')
     
     st.header('Seu valor comercial')
-    st.write("Um crédito de carbono equivale a R$175,00")
+    st.write('Em maio deste ano, o preço por crédito alcançou os 53 euros, equivalentes a 336 reais por tonelada, exibindo uma valorização exorbirtante de 260% desde os ultimos 3 anos.  O aumento vem sendo esponencial apenas por um fator : Oferta e Demanda')
+    st.write ('A pressão pela redução destes gases acaba tornando necessária a possibilidade de comprar créditos de outras empresas para compensar seu débito, porém, o que acontece é que a demanda pela compra de tal crédito é muito maior que sua oferta, inflando assim seu preço.')
+    
+    st.write("Abaixo está informada uma tabela referenciando a quantidade de créditos obtidos pela redução da emissão de alguns gases específicos, note que seu perigo ambiental aumenta seu peso em créditos.")
 
     #Coluna Informativa relacionada ao ganho de créditos relacionados à emissão reduzida.
     col1, col2 = st.columns(2)
